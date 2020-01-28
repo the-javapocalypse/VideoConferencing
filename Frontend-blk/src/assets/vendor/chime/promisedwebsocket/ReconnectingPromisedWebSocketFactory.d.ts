a099f57ee89e6d328878c0191ac4ce20bfe3f126
the-javapocalypse <muhammad.17ali@gmail.com>
2020-01-28 11:43:22 +0500

    Removed Junk

--- Frontend-blk/src/assets/vendor/chime/promisedwebsocket/ReconnectingPromisedWebSocketFactory.d.ts
+++
@@ -1,9 +0,0 @@
-import BackoffFactory from '../backoff/BackoffFactory';
-import PromisedWebSocket from './PromisedWebSocket';
-import PromisedWebSocketFactory from './PromisedWebSocketFactory';
-export default class ReconnectingPromisedWebSocketFactory implements PromisedWebSocketFactory {
-    private promisedWebSocketFactory;
-    private backoffFactory;
-    constructor(promisedWebSocketFactory: PromisedWebSocketFactory, backoffFactory: BackoffFactory);
-    create(url: string, protocols?: string | string[] | null, binaryType?: BinaryType): PromisedWebSocket;
-}
--- Frontend-blk/src/assets/vendor/chime/screensharefacade/ScreenShareFacadeObserver.d.ts
+++
@@ -1,7 +0,0 @@
-export default interface ScreenShareFacadeObserver {
-    didOpen?(event: Event): void;
-    didClose?(event: CloseEvent): void;
-    didStartScreenSharing?(): void;
-    didStopScreenSharing?(): void;
-    willReconnect?(): void;
-}
