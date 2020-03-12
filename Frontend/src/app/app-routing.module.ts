b23b085cf52fd6330611d65e9f4f19f04638b0a3
the-javapocalypse <muhammad.17ali@gmail.com>
2020-03-12 17:30:14 +0500

    Join init

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import {MeetingComponent} from './pages/video/meeting/meeting.component';
 import {AccountComponent} from './pages/auth/account/account.component';
 import {HomeComponent} from './pages/dashboard/home/home.component';
 import {AuthGuardService} from './services/guard/auth-guard.service';
+import {JoinComponent} from './pages/video/join/join.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
   { path: 'account', component: AccountComponent},
-  { path: 'join/:meeting_id', component: JoinTempComponent },
-  { path: 'meeting/:meeting_id', component: MeetingTempComponent},
-  { path: 'conference/:meeting_id', component: MeetingComponent},
+  { path: 'join_old/:meeting_id', component: JoinTempComponent },
+  { path: 'meeting_old/:meeting_id', component: MeetingTempComponent},
+  { path: 'join/:digest', component: JoinComponent},
+  { path: 'meeting/:meeting_id', component: MeetingComponent},
 ];
 
 @NgModule({
--- Frontend/src/app/pages/video/join/join.component.ts
+++ Frontend/src/app/pages/video/join/join.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-join',
-  templateUrl: './join.component.html',
-  styleUrls: ['./join.component.sass']
+    selector: 'app-join',
+    templateUrl: './join.component.html',
+    styleUrls: ['./join.component.sass']
 })
 export class JoinComponent implements OnInit {
 
-  constructor() { }
+    // Data variables
+    digest = '';
 
-  ngOnInit() {
-  }
+    constructor(private route: ActivatedRoute, private router: Router) {
+    }
+
+    ngOnInit() {
+        // Grab meeting id from url
+        this.digest = this.route.snapshot.paramMap.get('digest');
+
+    }
+
 
 }
