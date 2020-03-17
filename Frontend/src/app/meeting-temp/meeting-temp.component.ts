4460c92546c67c785bf4491674bd2ba30c3052b9
the-javapocalypse <muhammad.17ali@gmail.com>
2020-03-17 16:51:56 +0500

    Latest build

--- Frontend/src/app/meeting-temp/meeting-temp.component.ts
+++ Frontend/src/app/meeting-temp/meeting-temp.component.ts
@@ -490,7 +490,7 @@ export class MeetingTempComponent implements OnInit, OnDestroy {
     leaveMeeting() {
         this.chime.leave();
         this.subscribe.unsubscribe();
-        this.router.navigate(['/join/' + this.meetingName]);
+        this.router.navigate(['/']);
     }
 
     // Display Toast Message
